dc696fa5fe0a3ed9e74cc6cbeb935474b06e16d2
zMarcxzz <81764164+zMarcxzz@users.noreply.github.com>
2022-10-16 19:56:22 -0300

    Create Lib.lua

---
+++ Lib.lua
@@ -0,0 +1,37 @@
+local module = {}
+module.__index = module
+
+local draw = Drawing.new
+
+function module.new(points,position,distance)
+
+    if points < 2 then return error('The min of points are 2') end
+    
+    local self = setmetatable({points={}},module)
+    
+    for i = 1,points-1 do
+        local new = draw('Line')
+        
+        new.From = position+(distance*i)
+        new.To = position+(distance*(i+1))
+        
+        new.Visible = true
+        
+        self.points[i] = new
+    end
+        
+    return self
+end
+
+function module:update(i,y)
+
+    if not self.points[i] then return error('The point doesnt exists') end
+    
+    self.points[i].From = y
+    
+    if self.points[i-1] then
+        self.points[i-1].To = y
+    end
+end
+
+return module
