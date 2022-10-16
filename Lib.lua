e92302b102beeabc727510ee3cfdc23e1a51131b
zMarcxzz <81764164+zMarcxzz@users.noreply.github.com>
2022-10-16 20:50:21 -0300

    Update Lib.lua

--- Lib.lua
+++ Lib.lua
@@ -33,13 +33,21 @@ function module.new(points,position,distance)
 end
 
 function module:update(i,y)
-
+    
+    if (not self.points[i]) and self.points[i-1] then
+        
+        self.points[i-1].From = Vector2.new(self.origin.X,self.origin.Y+y) + (self.distance*i)
+        
+        return
+    end
+        
+    
     if not self.points[i] then return error('The point doesnt exists') end
     
     self.points[i].From = Vector2.new(self.origin.X,self.origin.Y+y) + (self.distance*i)
     
     if self.points[i-1] then
-        self.points[i-1].To = Vector2.new(self.origin.X,self.origin.Y+y) + (self.distance*i)
+        self.points[i-1].To = Vector2.new(self.origin.X,self.origin.Y+y) + (self.distance*(i-1))
     end
 end
 
