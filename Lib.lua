d56f12f18c33c7d13987fdcc26e63dfa65623f66
zMarcxzz <81764164+zMarcxzz@users.noreply.github.com>
2022-10-16 20:41:54 -0300

    Update Lib.lua

--- Lib.lua
+++ Lib.lua
@@ -3,11 +3,13 @@ module.__index = module
 
 local draw = Drawing.new
 
+local circles = {}
+
 function module.new(points,position,distance)
 
     if points < 2 then return error('The min of points are 2') end
     
-    local self = setmetatable({origin=position,points={}},module)
+    local self = setmetatable({origin=position,distance=distance,points={}},module)
     
     for i = 1,points-1 do
         local new = draw('Line')
@@ -16,6 +18,13 @@ function module.new(points,position,distance)
         new.To = position+(distance*(i+1))
         
         new.Visible = true
+        
+        local circle = draw('Circle')
+        
+        circle.Radius = 10
+        circle.Visible = true
+        
+        circles[new] = circle
         
         self.points[i] = new
     end
@@ -27,10 +36,10 @@ function module:update(i,y)
 
     if not self.points[i] then return error('The point doesnt exists') end
     
-    self.points[i].From = Vector2.new(self.origin.X,self.origin.Y+y)
+    self.points[i].From = Vector2.new(self.origin.X,self.origin.Y+y) + (self.distance*i)
     
     if self.points[i-1] then
-        self.points[i-1].To = Vector2.new(self.origin.X,self.origin.Y+y)
+        self.points[i-1].To = Vector2.new(self.origin.X,self.origin.Y+y) + (self.distance*i)
     end
 end
 
