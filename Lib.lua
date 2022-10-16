7d1c13232b9948a6e017699d459c64f6e7e573fd
zMarcxzz <81764164+zMarcxzz@users.noreply.github.com>
2022-10-16 20:00:56 -0300

    Update Lib.lua

--- Lib.lua
+++ Lib.lua
@@ -7,7 +7,7 @@ function module.new(points,position,distance)
 
     if points < 2 then return error('The min of points are 2') end
     
-    local self = setmetatable({points={}},module)
+    local self = setmetatable({origin=position,points={}},module)
     
     for i = 1,points-1 do
         local new = draw('Line')
@@ -27,10 +27,10 @@ function module:update(i,y)
 
     if not self.points[i] then return error('The point doesnt exists') end
     
-    self.points[i].From = y
+    self.points[i].From = Vector2.new(self.origin.X,y)
     
     if self.points[i-1] then
-        self.points[i-1].To = y
+        self.points[i-1].To = Vector2.new(self.origin.X,y)
     end
 end
 
